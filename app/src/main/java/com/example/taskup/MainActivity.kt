188baa3a7d0c92953215c78f42b11a37580840ab
Andrea Dayo <andrealouisedayo@gmail.com>
2023-11-20 02:48:52 +0800

    Added bottom navigation and theme

--- app/src/main/java/com/example/taskup/MainActivity.kt
+++ app/src/main/java/com/example/taskup/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.taskup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.taskup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Now you can find the NavController using the inflated binding
+        navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
+
+        // Set up bottom navigation with the NavController
+        setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
