7b332c5402d7b8eecc9f83bde00cfc3943f69612
Andrea Dayo <andrealouisedayo@gmail.com>
2023-11-20 03:18:19 +0800

    Fixed theme and tested dark mode switch

--- app/src/main/java/com/example/taskup/MainActivity.kt
+++ app/src/main/java/com/example/taskup/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.taskup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.taskup.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,11 +20,34 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Now you can find the NavController using the inflated binding
         navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
-
-        // Set up bottom navigation with the NavController
         setupWithNavController(binding.bottomNavigationView, navController)
+
+        // Enable DayNight mode
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        val darkModeSwitch = findViewById<Switch>(R.id.darkModeSwitch)
+
+        // Check the current mode and set the switch accordingly
+        darkModeSwitch.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val transition = AutoTransition()
+            transition.duration = 500 // Set the duration of the transition in milliseconds
+
+            // Use TransitionManager to animate the changes
+            TransitionManager.beginDelayedTransition(findViewById(android.R.id.content), transition)
+
+            if (isChecked) {
+                // Enable dark mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                // Enable light mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            // Recreate the activity to apply the new mode
+            recreate()
+        }
     }
 }
