a191524ca53e5b8bb21a43b3a41c263b21be81c4
Andrea Dayo <andrealouisedayo@gmail.com>
2023-11-27 18:15:05 +0800

    Frontend: Landing, Signup, Login

--- app/src/main/java/com/example/taskup/MainActivity.kt
+++ app/src/main/java/com/example/taskup/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.taskup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Switch
+import android.widget.Button
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.taskup.databinding.ActivityMainBinding
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.transition.AutoTransition
-import androidx.transition.TransitionManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,37 +13,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
-        setupWithNavController(binding.bottomNavigationView, navController)
-
-        // Enable DayNight mode
-        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-        val darkModeSwitch = findViewById<Switch>(R.id.darkModeSwitch)
-
-        // Check the current mode and set the switch accordingly
-        darkModeSwitch.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
-
-        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
-            val transition = AutoTransition()
-            transition.duration = 500 // Set the duration of the transition in milliseconds
-
-            // Use TransitionManager to animate the changes
-            TransitionManager.beginDelayedTransition(findViewById(android.R.id.content), transition)
-
-            if (isChecked) {
-                // Enable dark mode
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            } else {
-                // Enable light mode
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-
-            // Recreate the activity to apply the new mode
-            recreate()
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+        btnGetStarted.setOnClickListener {
+            val i = Intent(this,SignupActivity::class.java)
+            startActivity(i)
         }
     }
+
+
 }
